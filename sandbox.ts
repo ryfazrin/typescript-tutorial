e18554c3e66faaf1e81dfe454a5e7ce2da4a64e9
ryfazrin <riansera515@gmail.com>
2022-07-13 11:37:57 +0800

    Lesson 4: Objects & Arrays

--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,37 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-// character = 20;
-character = 'luigi';
+names.push('ryan');
+// names.push(3);
+// names[0] = 3;
 
-// age = 'yoshi';
-age = 40;
+let numbers = [10, 20, 30, 40];
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[1] = 'shaun';
 
-const circle = (diameter: number) => {
-  return diameter * Math.PI;
+let mixed = ['ken', 4, 'chun', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryan';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  skills: []
 }
-
-// console.log(circle('hello'));
-console.log(circle(7.5));
