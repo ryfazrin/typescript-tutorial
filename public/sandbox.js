63b51df16bef014f7bc04bc8de19a9ed72574dfe
ryfazrin <riansera515@gmail.com>
2022-07-17 11:55:13 +0800

    Lesson 8: Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('test');
+// let greet = () => {
+//   console.log('hello, world');
+// }
+// greet = 'hello';
+let greet;
+greet = () => {
+    console.log('hello, again');
+};
+const add = (a, b = 10, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+// result = 'something else';
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,25 @@
-console.log('test');
+// let greet = () => {
+//   console.log('hello, world');
+// }
+
+// greet = 'hello';
+
+let greet: Function;
+
+greet = () => {
+  console.log('hello, again');
+}
+
+const add = (a: number, b: number = 10, c?: number|string) => {
+  console.log(a + b);
+  console.log(c);
+}
+
+add(5, 10, '20');
+
+const minus = (a: number, b: number) => {
+  return a + b;
+}
+
+let result = minus(10, 7);
+// result = 'something else';
